730bbf68ac020ebdbe88db8e2f8e687e10edaeaf
zarbo-ian <58311312+zarbo-ian@users.noreply.github.com>
2025-04-01 18:12:00 -0300

    a

--- Neo_Racing/Assets/Scripts/ElementShake.cs
+++ Neo_Racing/Assets/Scripts/ElementShake.cs
@@ -1,19 +1,15 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 
 public class ElementShake : MonoBehaviour
 {
-    
-    
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static ElementShake instance;
+    private void Awake() => instance = this;
+
+    private void OnShake(float duration, float strenght)
     {
-        
+        transform.DOShakePosition(duration, strenght);
     }
 
-    public void ShakeMe()
-    {
-        
-    }
-    
 }
